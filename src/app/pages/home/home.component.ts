71b019be91bdf6f6406b679ff18500509f3b25e7
Hugo Calzado <hcalzado@gmail.com>
2024-12-14 13:45:23 +0100

    first commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonButton,
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.scss'],
+  standalone: true,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonButton,
+  ],
+})
+export class HomeComponent {
+  constructor() {}
+}
